32b6f983f80271802732e50a7fdbf28eb071ccbe
Serhii <serhii.synohub@gmail.com>
2017-01-04 17:17:34 +0200

    ugly FAB

--- app/src/main/java/ua/kiev/sergiosiniy/smsfilter/activities/MainActivity.java
+++ app/src/main/java/ua/kiev/sergiosiniy/smsfilter/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package ua.kiev.sergiosiniy.smsfilter.activities;
 
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_start_service);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+            fab.setElevation(4);
+            fab.setAnimation();
+        }
     }
 }
