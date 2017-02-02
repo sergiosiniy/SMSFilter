a5af2ae73c79753e9e05a517f1b271dc27d5a23a
Serhii Synohub <serhii.synohub@gmail.com>
2017-02-02 17:50:14 +0200

    toolbar layout

--- app/src/main/java/ua/kiev/sergiosiniy/smsfilter/activities/MainActivity.java
+++ app/src/main/java/ua/kiev/sergiosiniy/smsfilter/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import ua.kiev.sergiosiniy.smsfilter.R;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbarSetup();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_start_service);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +47,22 @@ public class MainActivity extends AppCompatActivity {
     private void stopSMSFilterService(){
         stopService(serviceIntent);
         isRunning=false;
+    }
+
+    private void toolbarSetup(){
+        Toolbar actionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        actionBarToolbar.setNavigationIcon(R.mipmap.ic_menu_white_36dp);
+        actionBarToolbar.setNavigationContentDescription(R.string.navigation_icon_description);
+        actionBarToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        setSupportActionBar(actionBarToolbar);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP) {
+            actionBarToolbar.setElevation(4);
+        }
+
     }
 }
